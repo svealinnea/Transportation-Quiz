7cda8b1d503d6f5492658aac5d93978a48503702
Svea Wade <svealinneawade@gmail.com>
2020-10-27 14:59:36 -0700

    added last but of questions

--- Transportation-Survey/js/scripts.js
+++ Transportation-Survey/js/scripts.js
@@ -6,6 +6,16 @@ $(document).ready(function() {
       const workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
   });
-  $('transportation_survey').hide();
- });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      const funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
+  });
+  let userResponses = [];
+  $("input:checkbox[name=work-transportation]:checked").each(function(){
+      const workTransportationMode = $(this).val();
+      userResponses.push(workTransportationMode);
+    });
 });
